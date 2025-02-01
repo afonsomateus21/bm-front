129ec9cf276de9484d75e1bbeac43e62ceb3eb83
afonsomateus21 <afonsomateus.dev@gmail.com>
2025-02-01 00:19:35 -0300

    feat: cria botao secundario utilizado na aplicacao

---
+++ src/components/SecondaryButton.tsx
@@ -0,0 +1,17 @@
+import { ButtonProps } from "../types/button-props";
+
+export function SecondaryButton({ icon, ...rest }: ButtonProps) {
+	return(
+		<button 
+			{...rest}
+			className="bg-secondary w-full h-16 flex items-center justify-center rounded-xl relative"
+		>
+			<div className="bg-tertiary h-full w-20 absolute left-0 rounded-xl flex items-center justify-center">
+				{ icon }
+			</div>
+			<strong className="text-white text-xl">
+				Conta Google
+			</strong>
+		</button>
+	);
+}
