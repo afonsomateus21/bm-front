7cac75c582ae1242de84832cae2268d6d172f5a6
afonsomateus21 <afonsomateus.dev@gmail.com>
2025-02-01 00:21:56 -0300

    feat: adiciona estilos de login com google e criar conta

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,6 +2,7 @@ import Shape from '../assets/shape.svg';
 import Logo from '../assets/logo.svg';
 import { CustomInput } from '../components/CustomInput';
 import { PrimaryButton } from '../components/PrimaryButton';
+import { SecondaryButton } from '../components/SecondaryButton';
 
 export function Login() {
   return (
@@ -13,13 +14,13 @@ export function Login() {
         />
       </header>
 
-      <main className='h-full flex flex-col items-center p-4'>
+      <main className='h-full flex flex-col items-center p-4 relative'>
         <img 
           className='size-[250px]'
           src={ Logo } 
         />
 
-        <div className='w-full flex flex-col items-center gap-4 flex-1'>
+        <div className='w-full flex flex-col items-center gap-4'>
           <div className="w-full max-w-sm flex flex-col">
             <label 
               htmlFor='email'
@@ -51,9 +52,31 @@ export function Login() {
             </span>
           </div>
 
-          
-
           <PrimaryButton />
+
+          <div className="w-full flex items-center">
+            <div className="w-full border-b border-black relative">
+              <span className="absolute left-1/2 -translate-x-1/2 -top-3 bg-primary px-5 whitespace-nowrap text-secondary">
+                Ou entrar com
+              </span>
+            </div>
+          </div>
+
+          <SecondaryButton />
+        </div>
+
+        <div className='h-32 w-full bg-tertiary absolute bottom-0 rounded-t-2xl flex items-center justify-center'>
+          <span className='text-primary text-lg'>
+            Ainda não tem uma conta? Faça 
+            <strong>
+              <a 
+                href=""
+                className='ml-1 underline decoration-solid'
+              >
+                seu cadastro
+              </a>
+            </strong>
+          </span>
         </div>
       </main>
     </>
