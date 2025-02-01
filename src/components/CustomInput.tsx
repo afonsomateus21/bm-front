ea51d7e138e63df8ccfa8c8c18eb2ef5531930bd
Afonso Mateus <afonsomateus.dev@gmail.com>
2025-02-01 13:20:03 -0300

    refac: adiciona verificacao para mostrar ou nao a senha

--- src/components/CustomInput.tsx
+++ src/components/CustomInput.tsx
@@ -1,15 +1,27 @@
 import { Ref, forwardRef } from "react";
 import { InputProps } from "../types/input-props";
 
-function Input({ errors, ...rest }: InputProps, ref: Ref<HTMLInputElement>) {
+function Input({ 
+  icon = null, setShowPassword, showPassword, errors, ...rest 
+}: InputProps, ref: Ref<HTMLInputElement>) {
   return (
     <div>
-      <div className="w-full max-w-sm mx-auto rounded-4xl md:w-64 bg-white h-14 flex items-center px-3 border-2 border-secondary">
+      <div className="w-full max-w-sm mx-auto relative rounded-4xl md:w-64 bg-white h-14 flex items-center px-3 border-2 border-secondary">
         <input 
           {...rest}
           className="outline-none border-none w-full p-4" 
           ref={ref}
         />
+        <div 
+          className="absolute right-2"
+          onClick={ () => setShowPassword && setShowPassword(!showPassword) }
+        >
+          {
+            icon !== null
+            ? icon
+            : null
+          }
+        </div>
       </div>
 
       <p className="text-red-500"> {errors} </p>
