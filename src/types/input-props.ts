04ebf276901dcc22e9201db00c5563c169875bb3
Afonso Mateus <afonsomateus.dev@gmail.com>
2025-02-01 13:18:18 -0300

    refac: adiciona novas propriedades na interface InputProps

--- src/types/input-props.ts
+++ src/types/input-props.ts
@@ -1,5 +1,8 @@
-import { InputHTMLAttributes } from "react";
+import { InputHTMLAttributes, ReactNode } from "react";
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  icon?: ReactNode | string;
+  showPassword?: boolean;
+  setShowPassword?: (value: boolean) => void;
   errors?: string | undefined;
 }
