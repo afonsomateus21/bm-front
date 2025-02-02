2a870ff88c4f18bb52f2d5951558681db9a2e099
Afonso Mateus <afonsomateus.dev@gmail.com>
2025-02-01 23:54:49 -0300

    feat: adiciona rota de criacao de usuario

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router";
 import { Login } from "./pages/Login";
+import { UserRegister } from "./pages/UserRegister";
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Login />,
   }, 
+  {
+    path: '/register',
+    element: <UserRegister />
+  }
 ])
