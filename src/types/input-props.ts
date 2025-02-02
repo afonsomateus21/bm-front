86410a75dcb42d3e426888163693baf73c9a5c8f
Afonso Mateus <afonsomateus.dev@gmail.com>
2025-02-01 23:51:43 -0300

    feat: adiciona a propriedade title no tipagem do Input

--- src/types/input-props.ts
+++ src/types/input-props.ts
@@ -1,6 +1,7 @@
 import { InputHTMLAttributes, ReactNode } from "react";
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  title: string;
   icon?: ReactNode | string;
   showPassword?: boolean;
   setShowPassword?: (value: boolean) => void;
