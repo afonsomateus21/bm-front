33ceb7685db140efef0b9ca29f3b63ac5a6a9f28
Afonso Mateus <afonsomateus.dev@gmail.com>
2025-02-01 23:52:15 -0300

    refac: componentiza toda a estrutura do input

--- src/components/CustomInput.tsx
+++ src/components/CustomInput.tsx
@@ -2,12 +2,19 @@ import { Ref, forwardRef } from "react";
 import { InputProps } from "../types/input-props";
 
 function Input({ 
-  icon = null, setShowPassword, showPassword, errors, ...rest 
+  title, icon = null, setShowPassword, showPassword, errors, ...rest 
 }: InputProps, ref: Ref<HTMLInputElement>) {
   return (
-    <div>
+    <div className="w-full max-w-sm flex flex-col">
+      <label 
+        htmlFor={ title.split(' ')[0].toLowerCase() }
+        className='text-tertiary font-bold'
+      >
+        { title }
+      </label>
       <div className="w-full max-w-sm mx-auto relative rounded-4xl md:w-64 bg-white h-14 flex items-center px-3 border-2 border-secondary">
         <input 
+          id={ title.split(' ')[0].toLowerCase() }
           {...rest}
           className="outline-none border-none w-full p-4" 
           ref={ref}
