66270923b14638c06f956d6e747b5d250d371afe
Afonso Mateus <afonsomateus.dev@gmail.com>
2025-02-03 00:20:56 -0300

    refac: remove campos showPassword e setShowPassword

--- src/types/input-props.ts
+++ src/types/input-props.ts
@@ -3,7 +3,5 @@ import { InputHTMLAttributes, ReactNode } from "react";
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   title: string;
   icon?: ReactNode | string;
-  showPassword?: boolean;
-  setShowPassword?: (value: boolean) => void;
   errors?: string | undefined;
 }
