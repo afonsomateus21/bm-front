c08b1bcc37b609a1e1157d66947a992478b12f5d
Afonso Mateus <afonsomateus.dev@gmail.com>
2025-02-02 22:38:47 -0300

    refac: altera o componente para que o titulo seja dinamico

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -1,13 +1,13 @@
 import { ButtonProps } from "../types/button-props";
 
-export function PrimaryButton({ ...rest }: ButtonProps) {
+export function PrimaryButton({ title, ...rest }: ButtonProps) {
   return(
     <button 
       {...rest}
       className="bg-secondary w-full h-16 flex items-center justify-center rounded-xl"
     >
       <strong className="text-white text-xl">
-        Entrar
+        { title }
       </strong>
     </button>
   );
