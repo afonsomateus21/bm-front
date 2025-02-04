bade5835263a6bb69eaea32f1b1c94eb82546e9a
Afonso Mateus <afonsomateus.dev@gmail.com>
2025-02-03 22:25:44 -0300

    fix: corrige o titulo do botao

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -80,11 +80,11 @@ export function Login() {
             errors={ errors?.password?.message }
           />
 
-          <PrimaryButton type='submit' title="Cadastrar" />
+          <PrimaryButton type='submit' title="Entrar" />
 
           <div className="w-full flex items-center">
             <div className="w-full border-b border-black relative">
-              <span className="absolute left-1/2 -translate-x-1/2 -top-3 bg-primary px-5 whitespace-nowrap text-secondary text-sm">
+              <span className="absolute left-1/2 -translate-x-1/2 -top-3 bg-primary px-5 whitespace-nowrap text-secondary text-lg">
                 ou entrar com
               </span>
             </div>
