6848cfdb9aa0cd3cc814ace26a64a64ea924c023
Afonso Mateus <afonsomateus.dev@gmail.com>
2025-02-03 23:36:41 -0300

    feat: cria o componente IconButton

---
+++ src/components/buttons/IconButton.tsx
@@ -0,0 +1,17 @@
+import { ButtonProps } from "../../types";
+
+export function IconButton({ icon, ...rest }: ButtonProps) {
+	return(
+		<button 
+			{...rest}
+			className="bg-secondary w-full h-16 flex items-center justify-center rounded-xl relative"
+		>
+			<div className="bg-tertiary h-full w-20 absolute left-0 rounded-xl flex items-center justify-center">
+				{ icon }
+			</div>
+			<strong className="text-white text-xl">
+				Conta Google
+			</strong>
+		</button>
+	);
+}
