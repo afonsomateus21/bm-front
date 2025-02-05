36fbf1d745df231e2258256d54a630eceea2306b
Afonso Mateus <afonsomateus.dev@gmail.com>
2025-02-05 00:58:56 -0300

    refac: substitui os titulos fixos pelo arquivo de traducoes

--- src/utils/validations/loginSchema.tsx
+++ src/utils/validations/loginSchema.tsx
@@ -1,9 +1,11 @@
+import { TFunction } from "i18next";
 import { object, string } from "yup";
 
-export const loginSchema = object({
-  email: string()
-    .required("O e-mail é obrigatório")
-    .matches(/^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/, "E-mail inválido"),
-  password: string()
-    .required("A senha é obrigatória")
-})
+export const loginSchema = (t: TFunction) =>
+  object({
+    email: string()
+      .required(t("Common.Form.Errors.Email.Required"))
+      .matches(/^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/, t("Common.Form.Errors.Email.Invalid")),
+    password: string()
+      .required(t("Common.Form.Errors.Password.Required"))
+  })
