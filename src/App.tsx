2833abeccb65e3da2b5d737d94a11351d00454f5
Afonso Mateus <afonsomateus.dev@gmail.com>
2025-02-05 00:57:32 -0300

    feat: utiliza o i18n no projeto

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { RouterProvider } from 'react-router';
 import './index.css'
 import { router } from './routes';
 import { PasswordProvider } from './contexts';
+import './i18n.ts';
 
 function App() {
   return (
