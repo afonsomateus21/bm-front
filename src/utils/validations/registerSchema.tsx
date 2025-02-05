0ec512ab4c61b85c4a8a99b3781614452bca221a
Afonso Mateus <afonsomateus.dev@gmail.com>
2025-02-05 00:59:09 -0300

    refac: substitui os titulos fixos pelo arquivo de traducoes

--- src/utils/validations/registerSchema.tsx
+++ src/utils/validations/registerSchema.tsx
@@ -1,18 +1,21 @@
+import { TFunction } from "i18next";
 import { object, string, ref } from "yup";
 
-export const registerSchema = object({
-  firstName: string()
-    .required("O nome é obrigatório"),
-  lastName: string()
-    .required("O sobrenome é obrigatório"),
-  email: string()
-    .required("O e-mail é obrigatório")
-    .matches(/^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/, "E-mail inválido"),
-  phone: string()
-    .required("O telefone é obrigatório"),
-  password: string()
-    .required("A senha é obrigatória"),
-  confirmPassword: string()
-    .required("A confirmação de senha é obrigatória")
-    .oneOf([ ref("password") ], "As senhas devem ser iguais")
-})
+
+export const registerSchema = (t: TFunction) =>
+  object({
+    firstName: string()
+      .required(t('Common.Form.Errors.FirstName.Required')),
+    lastName: string()
+      .required(t('Common.Form.Errors.LastName.Required')),
+    email: string()
+      .required(t('Common.Form.Errors.Email.Required'))
+      .matches(/^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/, t('Common.Form.Errors.Email.Invalid')),
+    phone: string()
+      .required(t('Common.Form.Errors.Phone.Required')),
+    password: string()
+      .required(t('Common.Form.Errors.Password.Required')),
+    confirmPassword: string()
+      .required(t('Common.Form.Errors.ConfirmPassword.Required'))
+      .oneOf([ ref("password") ], t('Common.Form.Errors.ConfirmPassword.Invalid'))
+  })
