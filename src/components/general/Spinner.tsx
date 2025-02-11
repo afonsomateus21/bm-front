0c11f9847e3cae8ef7f4f904c45aeb1089d90043
Afonso Mateus <afonsomateus.dev@gmail.com>
2025-02-10 22:44:12 -0300

    feat: cria componente de Spinner

---
+++ src/components/general/Spinner.tsx
@@ -0,0 +1,9 @@
+import MoonLoader from "react-spinners/MoonLoader";
+
+export function Spinner() {
+  return (
+    <div className="h-full flex justify-center items-center">
+      <MoonLoader color="#EF007F" />
+    </div>
+  );
+}
