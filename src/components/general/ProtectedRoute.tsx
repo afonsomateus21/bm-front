d1061c50fec0d57d2a3211f5a683846bd59980bd
Afonso Mateus <afonsomateus.dev@gmail.com>
2025-02-10 22:46:25 -0300

    refac: adiciona spinner para mostrar que a tela de esta carregando

--- src/components/general/ProtectedRoute.tsx
+++ src/components/general/ProtectedRoute.tsx
@@ -1,10 +1,13 @@
 import { Navigate, Outlet } from "react-router";
 import { useAuth } from "../../hooks";
+import { Spinner } from "./Spinner";
 
 export function ProtectedRoute() {
   const { user, loading } = useAuth();
 
-  if (loading) return null;
+  if (loading) {
+    return <Spinner />
+  }
 
   if (!user) return <Navigate to="/login" replace />;
 
