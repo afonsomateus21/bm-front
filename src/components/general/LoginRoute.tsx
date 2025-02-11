47bf6f298a93efe59f6e1516b44fb695fbeb368d
Afonso Mateus <afonsomateus.dev@gmail.com>
2025-02-10 23:13:23 -0300

    refac: adiciona a cor do Spinner

--- src/components/general/LoginRoute.tsx
+++ src/components/general/LoginRoute.tsx
@@ -6,7 +6,7 @@ export function LoginRoute() {
   const { user, loading } = useAuth();
 
   if (loading) {
-    return <Spinner />
+    return <Spinner color="#EF007F" />
   }
 
   if (user) return <Navigate to="/home" replace />;
