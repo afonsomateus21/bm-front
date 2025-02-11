5508feb872cb1e286000b791c4741e21e2cb791c
Afonso Mateus <afonsomateus.dev@gmail.com>
2025-02-10 23:14:02 -0300

    refac: modifica o componente para aceitar as propriedades do MoonLoader

--- src/components/general/Spinner.tsx
+++ src/components/general/Spinner.tsx
@@ -1,9 +1,10 @@
+import { LoaderSizeProps } from "react-spinners/helpers/props";
 import MoonLoader from "react-spinners/MoonLoader";
 
-export function Spinner() {
+export function Spinner({ ...rest }: LoaderSizeProps) {
   return (
     <div className="h-full flex justify-center items-center">
-      <MoonLoader color="#EF007F" />
+      <MoonLoader { ...rest } />
     </div>
   );
 }
