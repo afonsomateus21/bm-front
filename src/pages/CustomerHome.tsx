c8763754a9d70cee1e7819980f31bda849fe9e08
Afonso Mateus <afonsomateus.dev@gmail.com>
2025-02-11 23:43:42 -0300

    feat: estilizacao do header da pagina home do cliente

--- src/pages/CustomerHome.tsx
+++ src/pages/CustomerHome.tsx
@@ -1,13 +1,25 @@
-import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
 import { useAuth } from "../hooks";
+import { ProfilePhoto } from "../components";
 
 export function CustomerHome() {
   const { user } = useAuth();
+  const { t } = useTranslation();
 
-  useEffect(() => {
-    console.log(user);
-  }, []);
   return (
-    <h1>Home</h1>
+    <div className="p-7">
+      <header className="h-48 flex items-center justify-between">
+        <div>
+          <h1 className="text-2xl ">{ t('Common.WelcomeMessage') }</h1>
+          <strong className="text-4xl ">
+            { `${user?.firstName} ${user?.lastName}!` }
+          </strong>
+        </div>
+        <ProfilePhoto 
+          profileName={ t('Common.ProfileSubtitle') }
+          photoUrl={ user?.photo } 
+        />
+      </header>
+    </div>
   );
 }
